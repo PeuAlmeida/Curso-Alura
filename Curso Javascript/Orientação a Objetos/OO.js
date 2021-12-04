ea5b5b5a2984919a1678fd3b72b18f6d3385a283
Pedro <pedrogunsx@gmail.com>
2021-12-04 13:30:12 -0300

    Aprendendo OO

---
+++ Curso Javascript/Orientação a Objetos/OO.js
@@ -0,0 +1,55 @@
+class Cliente {
+    nome;
+    cpf;
+}
+
+class ContaCorrente {
+    agencia;
+    _saldo; // Padrão da comunidade para avisar que um atributo é privado
+    //#saldo = 0 > Private em discurssão 
+
+    depositar(valor) {
+        if(valor <= 0) 
+        return;
+        
+        this._saldo += valor
+    }
+
+    sacar(valor) {
+        if(this._saldo >= valor) {
+            this._saldo -= valor;
+            return valor;
+        }
+    } 
+}
+
+// const cliente1 = new Cliente();
+// const cliente2 = new Cliente();
+
+// cliente1.nome = "Pedro";
+// cliente1.cpf = 11122233309;
+
+// cliente2.nome = "Alice";
+// cliente2.cpf = 22233344409;
+
+const contaCorrenteDoPedro = new ContaCorrente();
+const ContaCorrenteDaAlice = new ContaCorrente();
+
+contaCorrenteDoPedro.agencia = 1001;
+contaCorrenteDoPedro.saldo = 0;
+
+contaCorrenteDoPedro.depositar(50);
+
+ContaCorrenteDaAlice.agencia = 1001;
+ContaCorrenteDaAlice.saldo = 0;
+
+// console.log(cliente1);
+// console.log(cliente2);
+
+console.log(contaCorrenteDoPedro);
+
+contaCorrenteDoPedro.sacar(25);
+console.log(contaCorrenteDoPedro)
+
+
+
---
+++ Curso Javascript/Orientação a Objetos/index.js
@@ -0,0 +1,14 @@
+const cliente1Nome = "Pedro";
+const cliente1Cpf = 111122233309;
+const cliente1Agencia = 1001;
+const clienteSaldo = 0;
+
+const cliente2Nome = "Alice";
+const cliente2Cpf = 44455566609;
+const cliente2Agencia = 1001;
+const cliente2Saldo = 0;
+
+
+
+console.log(cliente1Nome);
+
