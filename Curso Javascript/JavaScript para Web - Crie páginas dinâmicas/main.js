db519e5eec6a32cfb0be9c641da8de86e76a3cf5
Pedro Almeida <pedroalmeida.programador@gmail.com>
2022-06-02 16:59:15 -0300

    Javascript Web
    Parte 3 / 5

--- Curso Javascript/JavaScript para Web - Crie páginas dinâmicas/main.js
+++ Curso Javascript/JavaScript para Web - Crie páginas dinâmicas/main.js
@@ -1,1 +1,7 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+document.querySelector('.tecla_pom').onclick = tocaSomPom;
